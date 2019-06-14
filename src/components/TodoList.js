af013f29e0984260b91e0798143f558c7e94e17d
Carlos Encalada <carlos.encalada@crifa.com>
2019-06-14 09:32:00 -0500

    separating variable logic

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,17 +3,19 @@ import React, { Component } from 'react';
 class TodoList extends Component {
   constructor(props) {
     super(props);
+    this.state = {};
   }
   
   render() {
+    let todos = this.props.todos.map((item, i) => 
+          <li key={i}> {item.title} - {item.completed ? "done" : "not completed"} </li>
+        ),
+        name = this.state.isLoggedIn ? this.props.name : "Guest";
+
     return (
       <div>
-       <h1>Hello {this.props.name}!</h1>
-       <ol>
-          {this.props.todos.map((item, i) => 
-            <li key={i}>{item.title} - {item.completed ? "done" : "not completed"}</li>
-          )}
-       </ol>
+        <h1>Hello {name}!</h1>
+        <ol>{todos}</ol>
       </div>
     );
   }
