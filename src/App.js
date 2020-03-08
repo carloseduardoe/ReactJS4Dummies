89410f1e865f19a8168582a5c033727f04095edb
carloseduardoe <42809876+carloseduardoe@users.noreply.github.com>
2020-03-08 17:31:15 -0500

    Minor refactoring

--- src/App.js
+++ src/App.js
@@ -5,34 +5,33 @@ import {
     Switch
 } from 'react-router-dom';
 
-import Header from './components/Header';
-import Public from './components/Public';
-import NotFound from './components/NotFound';
-import UserSignUp from './components/UserSignUp';
-import UserSignIn from './components/UserSignIn';
-import UserSignOut from './components/UserSignOut';
-import Tutorials from './components/Tutorials';
+import Header        from './components/Header';
+import Public        from './components/Public';
 import Authenticated from './components/Authenticated';
+import TodoList      from './components/TodoList';
+import NotFound      from './components/NotFound';
+import UserSignUp    from './components/UserSignUp';
+import UserSignIn    from './components/UserSignIn';
+import UserSignOut   from './components/UserSignOut';
+import Tutorials     from './components/Tutorials';
 
 import './assets/styles/App.scss';
 import './assets/styles/Auth.css';
-
-import TodoList from './components/TodoList';
 
 export default class App extends Component {
     render() {
         return <Router>
             <Header />
             <Switch>
-              <Route exact path="/" component={Public} />
-              <Route path="/safe" component={Authenticated} />
-              <Route path="/todos" component={TodoList} />
-              <Route path="/login" component={UserSignIn} />
-              <Route path="/signup" component={UserSignUp} />
-              <Route path="/logout" component={UserSignOut} />
-              <Route path="/tutorials/:category/:item" component={Tutorials} exact/>
-              <Route path="/tutorials" component={Tutorials} />
-              <Route component={NotFound} />
+                <Route exact path = "/"                          component = { Public } />
+                <Route       path = "/safe"                      component = { Authenticated } />
+                <Route       path = "/todos"                     component = { TodoList } />
+                <Route       path = "/login"                     component = { UserSignIn } />
+                <Route       path = "/signup"                    component = { UserSignUp } />
+                <Route       path = "/logout"                    component = { UserSignOut } />
+                <Route exact path = "/tutorials/:category/:item" component = { Tutorials } />
+                <Route       path = "/tutorials"                 component = { Tutorials } />
+                <Route                                           component = { NotFound } />
             </Switch>
         </Router>;
     }
